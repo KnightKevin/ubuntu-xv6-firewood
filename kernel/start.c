b15f632205211b7ac498b213bd368e6d3329f328
Simon <dd@dd.com>
2024-03-05 21:56:55 +0800

    mstatus

--- kernel/start.c
+++ kernel/start.c
@@ -3,10 +3,8 @@
 void main();
 
 void start() {
-
-    int a = 0;
-    int b = 1;
-    int c = a+b;
+    asm volatile("csrw mstatus, %0" : : "r" (0b100000000000));
+    
     // 通过mret跳转到到supervisor mode。其实是讨巧的做法，一般是从前一个Supervisor模式转换到Machine模式的调用返回，说人话： 就是s转到m，然后m现在做完事情了须要返回到s状态
     
     // 配置mepc寄存器，地址就是main.c中的main方法地址
